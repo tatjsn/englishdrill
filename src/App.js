9b5e10b1ef29291a728d11053148276d61076fd3
Taz Jackson <tatjsn@users.noreply.github.com>
2019-03-24 09:54:42 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import shuffle from 'lodash.shuffle';
 import './App.css';
 
-class App extends Component {
-  render() {
+function App({ db, speak }) {
+  const [score, setScore] = useState(-1);
+  const [items, setItems] = useState([]);
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    const ref = db
+      .collection('weeks')
+      .doc('hnfRsBB7ALdfyEgdj7wK');
+    return ref.onSnapshot((doc) => {
+      const data = doc.data();
+      setScore(data.score);
+      setItems(data.items);
+      if (questions.length === 0) {
+        setQuestions(shuffle([...data.items.keys()]));
+      }
+    });
+  }, []); // Do not rerun
+
+  if (score === -1 || items.length === 0 || questions.length === 0) {
+    console.log('show fallback');
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>Now loading...</div>
     );
   }
+
+  console.log('dbg', questions);
+  const q = questions[0];
+  const item = items[q];
+  const nonQs = shuffle([...items.keys()].filter(x => x !== q)).slice(0,2);
+  const answers = shuffle([q, ...nonQs]);
+
+  return (
+    <div>
+      <div>score is {score}.</div>
+      <img alt="question" width={200} height={200} src={item.image} onClick={() => speak(item.word)} />
+      {answers.map(a => items[a]).map(item => (
+        <li key={item.word}>{item.word}</li>
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+firebase.initializeApp({
+  apiKey: 'AIzaSyD9G7Mguzxb3hdzAtiEiWzM550DXknwcVU',
+  authDomain: 'englishdrill-43d5a.firebaseapp.com',
+  projectId: 'englishdrill-43d5a'
+});
+
+const db = firebase.firestore();
+
+const speak = voiceId => msg => new Promise((resolve) => {
+  const m = new SpeechSynthesisUtterance();
+  m.voice = window.speechSynthesis.getVoices()[voiceId];
+  m.text = msg;
+  m.onend = resolve;
+  window.speechSynthesis.speak(m);
+});
+
+ReactDOM.render(<App db={db} speak={speak(0)}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
